b0adaa303a5bff0cc1256cd34c963b48efbc9d65
Jaron Johnson <jaronjohnson14@gmail.com>
2022-03-08 10:09:04 -0700

    threw my image on the website

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import brickyard from "./assets/PXL_20220308_064811105.MP.jpg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={brickyard} className="App-logo" alt="jaron" />
+        <h2>Welcome</h2>
+        <h1>Future site of Jaron's Backyard Brick Oven</h1>
+        <p>Give us some time while we do some building!</p>
       </header>
     </div>
   );
