bbe60ee78e30422c62d3639490d7788f0ce040e9
Jaron Johnson <jaron@circleinapp.com>
2022-03-09 22:50:03 -0700

    adding auth to project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 import brickyard from "./assets/PXL_20220308_064811105.MP.jpg";
 import "./App.css";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore/lite";
+
+const firebaseConfig = {
+  apiKey: "API_KEY",
+  authDomain: "PROJECT_ID.firebaseapp.com",
+  // The value of `databaseURL` depends on the location of the database
+  databaseURL: "https://DATABASE_NAME.firebaseio.com",
+  projectId: "PROJECT_ID",
+  storageBucket: "PROJECT_ID.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID",
+  // For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
+  measurementId: "G-MEASUREMENT_ID",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
 
 function App() {
   return (
---
+++ src/firebaseConfig.js
@@ -0,0 +1,27 @@
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "firebase/firestore/lite";
+
+// const firebaseConfig = {
+//   apiKey: "API_KEY",
+//   authDomain: "PROJECT_ID.firebaseapp.com",
+//   // The value of `databaseURL` depends on the location of the database
+//   databaseURL: "https://DATABASE_NAME.firebaseio.com",
+//   projectId: "PROJECT_ID",
+//   storageBucket: "PROJECT_ID.appspot.com",
+//   messagingSenderId: "SENDER_ID",
+//   appId: "APP_ID",
+//   // For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
+//   measurementId: "G-MEASUREMENT_ID",
+// };
+
+// const firebaseApp = initializeApp(firebaseConfig);
+// const db = getFirestore(app);
+
+// // db example
+
+// // async function getCities(db) {
+// //   const citiesCol = collection(db, 'cities');
+// //   const citySnapshot = await getDocs(citiesCol);
+// //   const cityList = citySnapshot.docs.map(doc => doc.data());
+// //   return cityList;
+// // }
