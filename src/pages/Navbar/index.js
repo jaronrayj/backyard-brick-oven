22701fd7f17a0b234f4089df060d08b79925ee62
Jaron Johnson <jaron@circleinapp.com>
2022-06-01 23:55:07 -0600

    updated with pictures and text on how to find me. updated whole placeholder interface

--- src/pages/Navbar/index.js
+++ src/pages/Navbar/index.js
@@ -55,15 +55,21 @@ const Navbar = () => {
           <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
             <div className="relative flex items-center justify-between h-16">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                <img
+                  className="block h-14 mr-4"
+                  src={logo}
+                  alt="Backyard Brick Oven"
+                />
+                <h2 className="text-xl font-mono">Backyard Brick Oven</h2>
                 {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                {/* <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
                     <XIcon className="block h-6 w-6" aria-hidden="true" />
                   ) : (
                     <MenuIcon className="block h-6 w-6" aria-hidden="true" />
                   )}
-                </Disclosure.Button>
+                </Disclosure.Button> */}
               </div>
               <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="hidden sm:block sm:ml-6">
