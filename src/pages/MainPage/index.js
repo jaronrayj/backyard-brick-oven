6c6ef98271ce2a582dc379969e5be90b2179554a
Jaron Johnson <jaronjohnson14@gmail.com>
2024-02-21 16:50:28 -0700

    fixed large grid

--- src/pages/MainPage/index.js
+++ src/pages/MainPage/index.js
@@ -138,7 +138,7 @@ const MainPage = () => {
             </>
           )}
           <div className="font-bold text-xl mb-6 mt-6">Current Pizzas</div>
-          <div className="grid grid-cols-2 gap-4">
+          <div className="grid grid-cols-2 lg:grid-cols-3 gap-4">
             {activePizzas &&
               Object.entries(activePizzas).map(
                 ([key, value]) =>
